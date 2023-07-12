b31a842e6379bbe107fd9b4ad8416e8c7a07793e
Alexei Capsamun <caps.alex@mail.ru>
2023-07-12 15:46:41 +0300

    update task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num
+}
--- task21/Program.cs
+++ task21/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int x = Prompt("Введите точку X: ");
+int y = Prompt("Введите точку Y: ");
+int z = Prompt("Введите точку Z: ");
+
+
+if (x > 0 && y > 0 && z > 0)
+{
+    Console.WriteLine("I четверть");
+}
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("II четверть");
+}
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("III четверть");
+}
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("IV четверть");
+}
